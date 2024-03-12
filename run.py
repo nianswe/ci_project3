3f678052668a3d7a996014843789dc6c026a3d58
Niklas Andersson <nianswe@hotmail.com>
2024-03-12 14:24:43 +0000

    Add Place a piece function, used by Player 1 and 2, tested

--- run.py
+++ run.py
@@ -6,12 +6,11 @@ COLUMNS = 10
 
 """ Create Game Board  10 x 10 """
 def create_gameboard():
-    game_board = np.full((ROWS, COLUMNS), '.')
+    game_board = np.full((ROWS, COLUMNS), '   ')
     return game_board
 
 """ Print Game Board """
 def print_game_board(game_board):
-    game_board = create_gameboard()
     print(game_board)
 
 """ Place a Piece """
@@ -36,15 +35,21 @@ def main():
         if turn == 0:
             row = int(input(f"Player 1 make your choice row (0 - 9): "))
             column = int(input(f"Player 1 make your choice column (0 - 9): "))
+            """ Place a game piece """
+            place_piece(game_board, row, column, ' O ')
             turn += 1
-            print_game_board(game_board)  
+            """ Print Game Board """
+            print_game_board(game_board)
 
         else:
         # Player 2 Input    
             row = int(input(f"Player 2 make your choice row (0 - 9): "))
             column = int(input(f"Player 2 make your choice column (0 - 9): "))
+            """ Place a game piece """
+            place_piece(game_board, row, column, ' X ')
             turn += 1
             turn = turn % 2
+            """ Print Game Board """
             print_game_board(game_board)
 
 """ Run Game """
